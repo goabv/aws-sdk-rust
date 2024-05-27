c0c769a87b244498b7637473e1ebaf008c1d7e33
Abhinav Goyal <38365307+goabv@users.noreply.github.com>
2024-05-27 12:09:04 -0400

    testing parallel file reads

--- examples/examples/s3/src/bin/async-mpu.rs
+++ examples/examples/s3/src/bin/async-mpu.rs
@@ -59,7 +59,8 @@ async fn read_file_segment (i: usize, path: String,  starting_part_number: usize
     while (part_counter <= num_parts_thread){
         let mut read_total: usize = 0;
         let mut read_length: usize = 1;
-        let mut contents = vec![0_u8; chunk_size];
+        //let mut contents = vec![0_u8; chunk_size];
+        let mut contents = Vec::with_capacity(chunk_size);
         if (part_counter == num_parts_thread){
             part_size=last_part_size;
         }
@@ -71,22 +72,24 @@ async fn read_file_segment (i: usize, path: String,  starting_part_number: usize
         while (read_total < part_size) && (read_length != 0) {
             // Handle the case when the bytes remaining to be read are
             // less than the block size
+            read_length = chunk_size;
             if read_total + chunk_size > part_size {
                 contents.truncate(part_size - read_total);
+                read_length = part_size - read_total;
             }
 
-            /*
+
             unsafe {
                 // Create a slice from the uninitialized part of the buffer
-                let buffer_slice = slice::from_raw_parts_mut(buffer.as_mut_ptr(), length);
+                let buffer_slice = slice::from_raw_parts_mut(contents.as_mut_ptr(), read_length);
                 thread_file.read_exact(buffer_slice)?;
 
                 // Set the length of the buffer to the number of bytes read
-                buffer.set_len(length);
-            }*/
+                contents.set_len(read_length);
+            }
 
 
-            read_length = thread_file.read(&mut contents).expect("Couldn't read file");
+            //read_length = thread_file.read(&mut contents).expect("Couldn't read file");
 
             if (chunk_size!=part_size){
                 buffer.extend_from_slice(&contents[..read_length]);
