25f78e830ac30f5cb46352a9e4aa77c495df9653
Abhinav Goyal <38365307+goabv@users.noreply.github.com>
2024-05-27 12:36:38 -0400

    testing parallel file reads

--- examples/examples/s3/src/bin/async-mpu.rs
+++ examples/examples/s3/src/bin/async-mpu.rs
@@ -92,7 +92,7 @@ async fn read_file_segment (i: usize, path: String,  starting_part_number: usize
 
 
         //if (chunk_size!=part_size){
-            let byte_stream = ByteStream::from(Bytes::from(buffer));
+            let byte_stream = ByteStream::from(buffer);
         //}
             /*
         else {
