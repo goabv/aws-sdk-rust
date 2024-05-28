07ecc43175d92348800e989ee9c38cb4561fb866
Abhinav Goyal <38365307+goabv@users.noreply.github.com>
2024-05-28 13:54:06 -0400

    testing parallel file reads

--- examples/examples/s3/src/bin/async-mpu.rs
+++ examples/examples/s3/src/bin/async-mpu.rs
@@ -59,8 +59,8 @@ async fn read_memory_segment (i: usize, starting_part_number: usize, num_parts_t
         }
 
 
-            let deref_var = *GLOBAL_MEM_BUFF;
-            let byte_stream = ByteStream::from(deref_var);
+            let deref_var = &*GLOBAL_MEM_BUFF.clone();
+            let byte_stream = ByteStream::from(*deref_var);
 
 
 
